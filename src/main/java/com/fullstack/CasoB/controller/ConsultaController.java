ce03c7bb1ec81d710394a49c5daff62f238f7a70
pabloperezarcos <pabloperezarcos@gmail.com>
2024-04-12 22:13:49 -0400

    Correccion de comentarios

--- src/main/java/com/fullstack/CasoB/controller/ConsultaController.java
+++ src/main/java/com/fullstack/CasoB/controller/ConsultaController.java
@@ -1,7 +1,6 @@
 package com.fullstack.CasoB.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus; // No se usa, puedes eliminarlo
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
