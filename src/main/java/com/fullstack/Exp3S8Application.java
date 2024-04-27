8651d06b8a4a4d05617e97d734d86b3cfb35c0a7
pabloperezarcos <pabloperezarcos@gmail.com>
2024-04-26 20:21:32 -0400

    Update Exp3S8Application.java

--- src/main/java/com/fullstack/Exp3S8Application.java
+++ src/main/java/com/fullstack/Exp3S8Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication; // marca la
 import org.springframework.context.annotation.ComponentScan; // especifica los paquetes a escanear en busca de componentes de Spring
 
 @SpringBootApplication
-// Usamos la herramienta para que busque en los directorios "CasoA" y "CasoB" para poder trabajar con ambos casos con nuestro archivo main.
+// Usamos la herramienta para que busque en los directorios "CasoA" y "CasoB" para poder trabajar con ambos casos con nuestro archivo main. Exp3-S8
 @ComponentScan(basePackages = {"com.fullstack.CasoA", "com.fullstack.CasoB"})
 public class Exp3S8Application {
 
